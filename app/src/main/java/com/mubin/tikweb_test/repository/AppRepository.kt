0364cfc9653a3b64049f753bb10bba311459af80
Mubin <maksudmubin@gmail.com>
2022-09-07 18:48:10 +0600

    # files upload to firebase storage

--- app/src/main/java/com/mubin/tikweb_test/repository/AppRepository.kt
+++ app/src/main/java/com/mubin/tikweb_test/repository/AppRepository.kt
@@ -7,6 +7,4 @@ class AppRepository
 @Inject
 constructor(private val apiService: ApiService) {
 
-    //suspend fun getPhotos(page: Int) = apiService.getPhotos(page, 20, "popular", Constants.CLIENT_ID)
-
 }
--- app/src/main/java/com/mubin/tikweb_test/ui/MainActivity.kt
+++ app/src/main/java/com/mubin/tikweb_test/ui/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.mubin.tikweb_test.ui
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
-import com.mubin.tikweb_test.R
+import com.esafirm.imagepicker.features.ImagePicker
+import com.esafirm.imagepicker.model.Image
+import com.google.firebase.storage.FirebaseStorage
 import com.mubin.tikweb_test.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +28,112 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //https://github.com/Shakiba-e-nur/firebase_single_multiple_image_upload_retrieve
+        initClickListener()
+
+    }
+
+    private fun initClickListener() {
+        binding.btnSelectImages.setOnClickListener {
+
+            if (Build.VERSION.SDK_INT >= 23 ) {
+                if (isCheckPermission()){
+                    chooseImageToUpload()
+                }
+            } else {
+                isCheckPermissionBelow23()
+            }
+
+        }
+
+        binding.btnViewImages.setOnClickListener {
+            getImages()
+        }
+    }
+
+    private fun chooseImageToUpload() {
+        ImagePicker.create(this)
+            .folderMode(true)
+            .showCamera(true)
+            .toolbarFolderTitle("Select Folder")
+            .toolbarImageTitle("Select Images")
+            .includeVideo(false)
+            .limit(40)
+            .start()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
+
+            val imageList = ImagePicker.getImages(data)
+            val pathList = imageList.map { it.uri }
+            val filterImageList: MutableList<Image> = mutableListOf()
+
+            pathList.forEachIndexed { index, _ ->
+                filterImageList.add(imageList[index])
+            }
+
+            binding.tvImageSelectedCount.text = "${filterImageList.size} images selected for upload"
+            uploadImagesToFirebaseStorage(filterImageList)
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+    }
+
+    private fun uploadImagesToFirebaseStorage(filterImageList: MutableList<Image>) {
+
+        val imagefolder = FirebaseStorage.getInstance().reference.child("Image_Folder")
+
+        binding.progressBar.visibility = View.VISIBLE
+        
+        for (item in filterImageList) {
+
+            val individualImage: Uri = item.uri
+
+            val imageName = imagefolder.child("Image" + individualImage.lastPathSegment)
+
+            Log.d("ImageToBeUpload", "$imageName")
+
+            imageName.putFile(individualImage).addOnCompleteListener {
+                 if (it.isComplete){
+                     binding.progressBar.visibility = View.GONE
+                     Toast.makeText(this, "Upload Successful", Toast.LENGTH_SHORT).show()
+                     binding.tvImageSelectedCount.text = ""
+                 } else {
+                     binding.progressBar.visibility = View.GONE
+                     Toast.makeText(this, "Upload Failed", Toast.LENGTH_SHORT).show()
+                     binding.tvImageSelectedCount.text = ""
+                 }
+            }
+
+        }
+
+    }
+
+    private fun getImages() {
+
+        val databaseReference = FirebaseStorage.getInstance().getReference("Image_Folder")
+
+        databaseReference.parent?.child("Image_Folder")?.listAll()?.addOnCompleteListener { it ->
+            if (it.isSuccessful) {
+
+                val result = it.result
+
+                val items = result.items
+
+                for (i in items) {
+                    Log.d("Images", "${i.downloadUrl}")
+                }
+
+            } else {
+
+                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
 
     }
 
@@ -29,8 +142,9 @@ class MainActivity : AppCompatActivity() {
         val permissionWrite = PermissionChecker.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
         if (permissionRead == PermissionChecker.PERMISSION_GRANTED && permissionWrite == PermissionChecker.PERMISSION_GRANTED) {
-            //Session.isRead = true
-            //Session.isWrite = true
+
+            chooseImageToUpload()
+
         } else {
             Toast.makeText(this, "Storage Permission is needed.", Toast.LENGTH_SHORT).show()
         }
@@ -67,11 +181,10 @@ class MainActivity : AppCompatActivity() {
 
     private val requestMultiplePermissions = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
         permissions.entries.forEach { permission ->
-            if (permission.key == Manifest.permission.READ_EXTERNAL_STORAGE) {
-                //Session.isRead = permission.value
-            }
-            if (permission.key == Manifest.permission.WRITE_EXTERNAL_STORAGE) {
-                //Session.isWrite = permission.value
+            if (permission.key == Manifest.permission.READ_EXTERNAL_STORAGE && permission.key == Manifest.permission.WRITE_EXTERNAL_STORAGE) {
+
+                chooseImageToUpload()
+
             }
         }
     }
